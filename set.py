7375b398a25b3df4ea48d07e48f715c235ebf807
SheikLaisha <90794575+SheikLaisha@users.noreply.github.com>
2021-09-21 20:50:20 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,7 @@
+E={0,2,4,6,8}
+N={1,2,3,4,5}
+print('The two sets E and N are:',E,N)
+print('Union of E and N :',E.union(N))
+print('Intersection of E and N :',E.intersection(N))
+print('Difference of E and N :',E.difference(N))
+print('Symmetric difference of E and N :',E.symmetric_difference(N))
